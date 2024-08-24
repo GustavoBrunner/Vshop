6caee532267271640e3c9e32545d1848224df11b
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2024-08-23 21:46:17 -0300

    Entities added

---
+++ VShop.ProductAPI/VShop.ProductAPI/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace VShop.ProductAPI.Models;
+
+public class Category
+{
+    public Category() 
+    {
+        CategoryId = Guid.NewGuid().ToString();
+    }
+    public string CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = string.Empty;
+
+    public ICollection<Product>? Products { get; set; } 
+}
---
+++ VShop.ProductAPI/VShop.ProductAPI/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VShop.ProductAPI.Models;
+
+public class Product
+{
+    public Product()
+    {
+        ProductId = Guid.NewGuid().ToString();
+    }
+
+    public string ProductId { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public long Stock { get; set; }
+    public string? ImageUrl { get; set; }
+    public string CategoryId { get; set; }
+    [NotMapped]
+    public Category? Category { get; set; }
+
+}
