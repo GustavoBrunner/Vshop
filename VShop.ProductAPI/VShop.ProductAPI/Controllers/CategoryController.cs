7e34fdb83c6f9ce50b8cc5d8a8b925d7a7e4aa23
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2024-08-24 16:15:49 -0300

    Controllers created

---
+++ VShop.ProductAPI/VShop.ProductAPI/Controllers/CategoryController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using VShop.ProductAPI.DTOs;
+using VShop.ProductAPI.Services.Contracts;
+
+namespace VShop.ProductAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+    public CategoryController(ICategoryService categoryService) 
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateCategory([FromBody] CategoryDto categoryDto)
+    {
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+        
+        var category = _categoryService.GetCategoryByIdAsync(categoryDto.CategoryId);
+        if(category is not null)
+            return BadRequest("Category already exists!");
+
+        await _categoryService.AddCategoryAsync(categoryDto);
+        return Ok(categoryDto); 
+    }
+
+    [HttpGet("id:string")]
+    public async Task<ActionResult<CategoryDto>> GetCategory(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+            return BadRequest("No category informed");
+        var category = await _categoryService.GetCategoryByIdAsync(id);
+        
+        if (category is null)
+            return NotFound("Category not found!");
+
+        return Ok(category);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoryDto>>> GetAllCategories()
+    {
+        var entities = await _categoryService.GetAllCategoriesAsync();
+        if (entities is null)
+            return NotFound("No category found!");
+
+        return Ok(entities);
+    }
+
+    [HttpGet("/products")]
+    public async Task<ActionResult<IEnumerable<CategoryDto>>> GetAllCategoriesIncludeProducts()
+    {
+        var entities = await _categoryService.GetAllCategoriesIncludeProductsAsync();
+        if (entities is null)
+            return NotFound("No category found!");
+
+        return Ok(entities);
+    }
+
+    [HttpPut("{id:string}")]
+    public async Task<ActionResult> UpdateCategory([FromBody] CategoryDto categoryDto, string id)
+    {
+        if (!string.IsNullOrEmpty(id))
+            return BadRequest("No category informed!");
+        if (!ModelState.IsValid)
+            return BadRequest(categoryDto);
+
+        var category = await _categoryService.GetCategoryByIdAsync(id);
+        if (category is null)
+            return NotFound("Category not found!");
+        await _categoryService.UpdateCategoryAsync(category);
+
+        return Ok(category);
+    }
+    
+    [HttpDelete("{id:string}")]
+    public async Task<ActionResult> DeleteCategory(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+            return BadRequest("No category informed!");
+
+        bool result = await _categoryService.DeleteCategoryAsync(id);
+        if (!result)
+            return NotFound("Category does not exist!");
+
+        return Ok();
+    }
+
+}
---
+++ VShop.ProductAPI/VShop.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using VShop.ProductAPI.DTOs;
+using VShop.ProductAPI.Services.Contracts;
+
+namespace VShop.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        
+        [HttpPost] 
+        public async Task<ActionResult> CreateProduct([FromBody] ProductDto product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(product);
+
+            var productDto = await _productService.GetProductByIdAsync(product.ProductId);
+            if (productDto is not null)
+                return BadRequest("Product already exists!");
+
+            await _productService.AddNewProductAsync(product);
+            return Ok(product);
+        }
+
+        [HttpGet("{id:string}")]
+        public async Task<ActionResult<ProductDto>> GetById(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest("Product not informed!");
+            }
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product is null)
+            {
+                return NotFound("Product not found!");
+            }
+            return Ok(product);
+        }
+
+        [HttpDelete("{id:string}")]
+        public async Task<ActionResult> DeleteProduct(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return BadRequest("No product informed!");
+         
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product is null)
+                return NotFound("Product not found!");
+
+            await _productService.DeleteProductByIdAsync(product.ProductId);
+            return Ok(product);
+        }
+
+        [HttpPut("{id:string}")]
+        public async Task<ActionResult> UpdateProduct([FromBody] ProductDto product, string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return BadRequest("No product informed!");
+            if (!ModelState.IsValid)
+                return BadRequest(product);
+
+            var entity = await _productService.GetProductByIdAsync(id);
+            if (entity is null) 
+            {
+                return NotFound("Product not found!");
+            }
+            await _productService.UpdateProductAsync(entity);
+
+            return Ok(entity);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAll()
+        {
+            var products = _productService.GetAllProductsAsync();
+            if (products == null)
+                return NotFound("No products found!");
+            return Ok(products);
+        }
+
+        [HttpGet("/category")]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAllIncludeCategories()
+        {
+            var categories = _productService.GetAllProductsIncludeCategoryAsync();
+            if (categories == null)
+                return NotFound("No product found!");
+            return Ok(categories);
+        }
+    }
+}
