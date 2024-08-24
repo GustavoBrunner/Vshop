a2992e9df49adef47f3c1aa24c9eba54508ee1d3
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2024-08-23 21:47:54 -0300

    Migrations added, with a seed migration for Categories and Products

---
+++ VShop.ProductAPI/VShop.ProductAPI/Migrations/20240824000714_Initial.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.ProductAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    PK_category_id = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    category_name = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.PK_category_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    PK_product_id = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    price = table.Column<decimal>(type: "decimal(12,2)", precision: 12, scale: 2, nullable: false),
+                    name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    description = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    stock = table.Column<long>(type: "bigint", nullable: false, defaultValue: 100L),
+                    ImageUrl = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FK_category_id = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.PK_product_id);
+                    table.ForeignKey(
+                        name: "FK_products_categories_FK_category_id",
+                        column: x => x.FK_category_id,
+                        principalTable: "categories",
+                        principalColumn: "PK_category_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "PK_category_id", "category_name" },
+                values: new object[] { "3255b7f7-ec73-4911-8429-69f8dac47872", "Food" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "PK_category_id", "category_name" },
+                values: new object[] { "7e054430-8cd7-4615-9b46-545ad98faf55", "Scholar material" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "PK_category_id", "category_name" },
+                values: new object[] { "dcb1eea1-34f2-4043-b6c4-d3f53ea78b79", "Clothes" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_FK_category_id",
+                table: "products",
+                column: "FK_category_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
---
+++ VShop.ProductAPI/VShop.ProductAPI/Migrations/20240824001220_SeedProducts.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.ProductAPI.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder mB)
+        {
+            mB.Sql("INSERT INTO products(PK_product_id, price, name, description, stock, FK_category_id) " +
+                "VALUES('3B5864D4-D020-4755-AFAA-33C9EC907E11', 5.50, 'Mamão', '',  30, '3255b7f7-ec73-4911-8429-69f8dac47872') ");
+            
+            mB.Sql("INSERT INTO products(PK_product_id, price, name, description, stock, FK_category_id) " +
+                "VALUES('DA655CA7-31EC-481F-9DCB-68D249E7F770', 10.50, 'NoteBook',' ', 100, '7e054430-8cd7-4615-9b46-545ad98faf55') ");
+
+            mB.Sql("INSERT INTO products(PK_product_id, price, name, description, stock, FK_category_id) " +
+                "VALUES('06D259CC-E8F7-450E-B895-C541B76E934F', 0.50, 'Pen',' ', 100, '7e054430-8cd7-4615-9b46-545ad98faf55') ");
+
+            mB.Sql("INSERT INTO products(PK_product_id, price, name, description, stock, FK_category_id) " +
+                "VALUES('8F628592-7025-4FA0-9395-3414F0F223FF', 4.50, 'Paper (100)',' ', 100, '7e054430-8cd7-4615-9b46-545ad98faf55') ");
+
+
+            mB.Sql("INSERT INTO products(PK_product_id, price, name, description, stock, FK_category_id) " +
+                "VALUES('5D4678B6-2F0F-4048-A083-24B61DC71AB5', 7.50, 'Chicken meat',' ', 100, '3255b7f7-ec73-4911-8429-69f8dac47872') ");
+
+
+            mB.Sql("INSERT INTO products(PK_product_id, price, name, description, stock, FK_category_id) " +
+                "VALUES('E6208A4C-0890-4D40-BB44-6FE0DA747B3F', 210.50, 'Tech T-shirt',' ', 300, 'dcb1eea1-34f2-4043-b6c4-d3f53ea78b79') ");
+
+            mB.Sql("INSERT INTO products(PK_product_id, price, name, description, stock, FK_category_id) " +
+                "VALUES('F4F2756D-62E3-4F75-AE74-947E5F22B9CB', 110.50, 'Jeans',' ', 240, 'dcb1eea1-34f2-4043-b6c4-d3f53ea78b79') ");
+        }
+
+
+        protected override void Down(MigrationBuilder mB)
+        {
+            mB.Sql("DELETE FROM products");
+        }
+    }
+}
