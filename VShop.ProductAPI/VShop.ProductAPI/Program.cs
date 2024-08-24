e2cc0abab47c1cea81545088de889d27f1aa1a5f
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2024-08-23 21:49:08 -0300

    General .net files

---
+++ VShop.ProductAPI/VShop.ProductAPI/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using VShop.ProductAPI.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration
+    .GetConnectionString(nameof(ProductApiContext));
+
+builder.Services.AddDbContext<ProductApiContext>(options => 
+    options.UseMySql(connectionString,
+    ServerVersion.AutoDetect(connectionString)));
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
