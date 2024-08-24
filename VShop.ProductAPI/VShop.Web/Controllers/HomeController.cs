188bdb1c9ea06b99147bab96bbd9423cf0ef50ad
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2024-08-24 18:51:06 -0300

    Web app created

---
+++ VShop.ProductAPI/VShop.Web/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VShop.Web.Models;
+
+namespace VShop.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+    }
+}
